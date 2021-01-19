d95da2e8ca39dcbc99627fa56ca517dc420c4604
Lluis Sanchez <llsan@microsoft.com>
2021-01-20 00:44:10 +0100

    Add initial support for heat pumps

--- ecoforest-proxy/ecoforest-proxy.py
+++ ecoforest-proxy/ecoforest-proxy.py
@@ -13,8 +13,89 @@ DEFAULT_PORT = int(jdata['proxy_port'])
 host = str(jdata['ecoforest_host'])
 username = str(jdata['ecoforest_user'])
 passwd = str(jdata['ecoforest_pass'])
+heatertype = str(jdata['type']) if 'type' in jdata else 'stove'
+stove = heatertype == 'stove'
 
 ECOFOREST_URL = host + '/recepcion_datos_4.cgi'
+
+REGISTER_TYPE_DIGITAL = 1
+REGISTER_TYPE_INTEGER = 2
+REGISTER_TYPE_ANALOG = 3
+
+heat_pump_registers_2001 = {
+    61: { 'id':'estado_piscina', 't':REGISTER_TYPE_DIGITAL },
+    105: { 'id':'heating_status', 't':REGISTER_TYPE_DIGITAL },
+    208: { 'id':'dhw_heating', 't':REGISTER_TYPE_DIGITAL },
+    209: { 'id':'piscina', 't':REGISTER_TYPE_DIGITAL },
+    206: { 'id':'calefaccion_directa', 't':REGISTER_TYPE_DIGITAL },
+    249: { 'id':'calefaccion_inercia', 't':REGISTER_TYPE_DIGITAL },
+    207: { 'id':'refrigeracion_directa', 't':REGISTER_TYPE_DIGITAL },
+    250: { 'id':'refrigeracion_inercia', 't':REGISTER_TYPE_DIGITAL },
+    210: { 'id':'htr', 't':REGISTER_TYPE_DIGITAL },
+    211: { 'id':'recirc_acs', 't':REGISTER_TYPE_DIGITAL },
+}
+
+heat_pump_registers_2002 = {
+    1: { 'id':'heating_outlet_temperature', 't':REGISTER_TYPE_ANALOG },
+    2: { 'id':'heating_inlet_temperature', 't':REGISTER_TYPE_ANALOG },
+    3: { 'id':'heating_real_temperature_1', 't':REGISTER_TYPE_ANALOG },
+    4: { 'id':'outlet_heating_temperature', 't':REGISTER_TYPE_ANALOG },
+    8: { 'id':'dhw_temperature', 't':REGISTER_TYPE_ANALOG },
+    13: { 'id':'pcp_a', 't':REGISTER_TYPE_ANALOG },
+    14: { 'id':'pcc_a', 't':REGISTER_TYPE_ANALOG },
+    15: { 'id':'offset', 't':REGISTER_TYPE_ANALOG },
+    19: { 'id':'imp_pool', 't':REGISTER_TYPE_ANALOG },
+    11: { 'id':'outdoor_temperature', 't':REGISTER_TYPE_ANALOG },
+    22: { 'id':'temp_cal', 't':REGISTER_TYPE_ANALOG },
+    30: { 'id':'cop_value', 't':REGISTER_TYPE_ANALOG },
+    31: { 'id':'pf_value', 't':REGISTER_TYPE_ANALOG },
+    55: { 'id':'offset_inercia_cool', 't':REGISTER_TYPE_ANALOG },
+    58: { 'id':'offset_inercia_heat', 't':REGISTER_TYPE_ANALOG },
+    97: { 'id':'heating_set_temperature_1', 't':REGISTER_TYPE_ANALOG },
+    98: { 'id':'set_th1_1', 't':REGISTER_TYPE_ANALOG },
+    99: { 'id':'set_th2_1', 't':REGISTER_TYPE_ANALOG },
+    125: { 'id':'temp_ref_act', 't':REGISTER_TYPE_ANALOG },
+    120: { 'id':'set_th3_1', 't':REGISTER_TYPE_ANALOG },
+    123: { 'id':'set_th4_1', 't':REGISTER_TYPE_ANALOG },
+    126: { 'id':'temp_ref', 't':REGISTER_TYPE_ANALOG },
+    176: { 'id':'c_dg1', 't':REGISTER_TYPE_ANALOG },
+    177: { 'id':'c_dg2', 't':REGISTER_TYPE_ANALOG },
+    178: { 'id':'c_dg3', 't':REGISTER_TYPE_ANALOG },
+    179: { 'id':'c_dg4', 't':REGISTER_TYPE_ANALOG },
+    186: { 'id':'d_dg1', 't':REGISTER_TYPE_ANALOG },
+    187: { 'id':'d_dg2', 't':REGISTER_TYPE_ANALOG },
+    188: { 'id':'d_dg3', 't':REGISTER_TYPE_ANALOG },
+    189: { 'id':'d_dg4', 't':REGISTER_TYPE_ANALOG },
+    190: { 'id':'h_dg1', 't':REGISTER_TYPE_ANALOG },
+    191: { 'id':'h_dg2', 't':REGISTER_TYPE_ANALOG },
+    192: { 'id':'h_dg3', 't':REGISTER_TYPE_ANALOG },
+    193: { 'id':'h_dg4', 't':REGISTER_TYPE_ANALOG },
+    194: { 'id':'heating_real_temperature_2', 't':REGISTER_TYPE_ANALOG },
+    195: { 'id':'heating_real_temperature_3', 't':REGISTER_TYPE_ANALOG },
+    196: { 'id':'heating_real_temperature_4', 't':REGISTER_TYPE_ANALOG },
+    198: { 'id':'comp_on_t', 't':REGISTER_TYPE_ANALOG },
+    200: { 'id':'temp_dep_heat', 't':REGISTER_TYPE_ANALOG },
+    201: { 'id':'temp_dep_cool', 't':REGISTER_TYPE_ANALOG },
+    215: { 'id':'set_inercia_heat', 't':REGISTER_TYPE_ANALOG },
+    216: { 'id':'set_inercia_cool', 't':REGISTER_TYPE_ANALOG },
+    214: { 'id':'consigna_acs', 't':REGISTER_TYPE_ANALOG },
+    202: { 'id':'eer_value', 't':REGISTER_TYPE_ANALOG },
+    5033: { 'id':'reg_bc_a', 't':REGISTER_TYPE_ANALOG },
+    5034: { 'id':'reg_bp_a', 't':REGISTER_TYPE_ANALOG },
+    5066: { 'id':'heating_set_temperature_2', 't':REGISTER_TYPE_ANALOG },
+    5082: { 'id':'electric_energy', 't':REGISTER_TYPE_INTEGER },
+    5083: { 'id':'useful_heat_power', 't':REGISTER_TYPE_INTEGER },
+    5113: { 'id':'heating_set_temperature_3', 't':REGISTER_TYPE_ANALOG },
+    5185: { 'id':'useful_cool_power', 't':REGISTER_TYPE_INTEGER },
+    5206: { 'id':'heating_set_temperature_4', 't':REGISTER_TYPE_ANALOG },
+    5210: { 'id':'heating_regulation_2', 't':REGISTER_TYPE_ANALOG },
+    5209: { 'id':'heating_regulation_3', 't':REGISTER_TYPE_ANALOG },
+    5211: { 'id':'heating_regulation_4', 't':REGISTER_TYPE_ANALOG },
+    5271: { 'id':'zone_1_active_demand', 't':REGISTER_TYPE_INTEGER }, # 0=none, 1=heating, 2=refrigerate
+    5272: { 'id':'zone_2_active_demand', 't':REGISTER_TYPE_INTEGER }, # 0=none, 1=heating, 2=refrigerate
+    5273: { 'id':'zone_3_active_demand', 't':REGISTER_TYPE_INTEGER }, # 0=none, 1=heating, 2=refrigerate
+    5274: { 'id':'zone_4_active_demand', 't':REGISTER_TYPE_INTEGER } # 0=none, 1=heating, 2=refrigerate
+}
 
 if DEBUG:
     FORMAT = '%(asctime)-0s %(levelname)s %(message)s [at line %(lineno)d]'
@@ -108,8 +189,13 @@ class EcoforestServer(BaseHTTPRequestHandler):
         print(data)
         self.send(self.ecoforest_stats())
 
-
     def ecoforest_stats(self):
+        if stove:
+            return self.ecoforest_stats_stove()
+        else:
+            return self.ecoforest_stats_heatpump()
+
+    def ecoforest_stats_stove(self):
         stats = self.ecoforest_call('idOperacion=1002')
         reply = dict(e.split('=') for e in stats.text.split('\n')[:-1]) # discard last line ?
 
@@ -141,12 +227,59 @@ class EcoforestServer(BaseHTTPRequestHandler):
 
         return reply
 
+    def ecoforest_stats_heatpump(self):
+        reply = {}
+        self.ecoforest_query_registers(reply, 2001, 61, 25)
+        self.ecoforest_query_registers(reply, 2001, 101, 97)
+        self.ecoforest_query_registers(reply, 2001, 206, 45)
+        self.ecoforest_query_registers(reply, 2002, 5033, 2)
+        self.ecoforest_query_registers(reply, 2002, 5066, 18)
+        self.ecoforest_query_registers(reply, 2002, 5113, 31)
+        self.ecoforest_query_registers(reply, 2002, 5185, 27)
+        self.ecoforest_query_registers(reply, 2002, 5241, 34)
+        self.ecoforest_query_registers(reply, 2002, 5285, 12)
+        self.ecoforest_query_registers(reply, 2002, 1, 39)
+        self.ecoforest_query_registers(reply, 2002, 40, 19)
+        self.ecoforest_query_registers(reply, 2002, 97, 30)
+        self.ecoforest_query_registers(reply, 2002, 176, 29)
+        self.ecoforest_query_registers(reply, 2002, 214, 7)
+        return reply
+
+    def ecoforest_query_registers(self,dict,oper,ini,num):
+        stats = self.ecoforest_call('idOperacion=' + str(oper) + '&dir=' + str(ini) + '&num=' + str(num))
+        lines = stats.text.split('\n')
+        result = lines[0].split('=')[0]
+        data = lines[1].split('&')
+        heat_pump_registers = heat_pump_registers_2001 if oper == 2001 else heat_pump_registers_2002
+        for i in range(2,len(data)):
+            regid = ini + i - 2
+            if regid in heat_pump_registers:
+                reg = heat_pump_registers[regid]
+                dict[reg['id']] = self.convert_register_value(data[i],reg['t'])
+
+    def convert_register_value(self,value,rtype):
+        if rtype == REGISTER_TYPE_DIGITAL:
+            return int(value)
+        elif rtype == REGISTER_TYPE_INTEGER:
+            val = int(value,16)
+            return val if val <= 32768 else val - 65536
+        elif rtype == REGISTER_TYPE_ANALOG:
+            val = int(value,16)
+            return val/10 if val <= 32768 else (val - 65536) / 10
+        else:
+            return 0
+
+    def set_heating_status(self, status):
+        if status == "on":
+            self.ecoforest_call('idOperacion=2011&num=1&1')
+        elif status == "off":
+            self.ecoforest_call('idOperacion=2011&num=1&0')
 
     # queries the ecoforest server with the supplied contents and parses the results into JSON
     def ecoforest_call(self, body):
         if DEBUG: logging.debug('Request:\n%s' % (body))
         headers = { 'Content-Type': 'application/json' }
-        try: 
+        try:
             request = requests.post(ECOFOREST_URL, data=body, headers=headers, auth=HTTPBasicAuth(username, passwd), timeout=2.5)
             if DEBUG: logging.debug('Request:\n%s' %(request.url))
             if DEBUG: logging.debug('Result:\n%s' %(request.text))
@@ -192,6 +325,7 @@ class EcoforestServer(BaseHTTPRequestHandler):
             '/ecoforest/fullstats': self.stats,
             '/ecoforest/status': self.get_status,
             '/ecoforest/set_status': self.set_status,
+            '/ecoforest/set_heating_status': self.set_heating_status,
             '/ecoforest/set_temp': self.set_temp,
             '/ecoforest/set_power': self.set_power,
         }
